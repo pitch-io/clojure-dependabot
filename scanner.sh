d373f28125e87a2ea01eb284d7fdb7a1504fd396
Luigi Gubello <luigi@pitch.io>
2023-02-01 13:55:08 +0100

    Add GA Summary

--- scanner.sh
+++ scanner.sh
@@ -14,6 +14,59 @@ dependency_tree_summary () {
     } >> "$GITHUB_STEP_SUMMARY"
 }
 
+vulnerabilities_summary () {
+    gh auth login --with-token <<<"$GITHUB_TOKEN"
+    vul_page=$(gh api -H "Accept: application/vnd.github+json" /repos/$GITHUB_REPOSITORY/dependabot/alerts --paginate)
+    mapfile -t info_pack < <(jq -r --arg MANIFEST $1 '.[] | select(.dependency.manifest_path == $MANIFEST and .state == "open") | (.number|tostring) + "|" + .security_vulnerability.package.name + "|" + .security_vulnerability.severity + "|" + .security_advisory.ghsa_id + "|" + .security_advisory.cve_id + "|" + .security_vulnerability.first_patched_version.identifier + "|"' <<< "${vul_page}")
+    for i in "${info_pack[@]}"
+    do
+        IFS='|' read -a array_i <<< "$i" 
+        cd "/${1/'pom.xml'/''}"
+        dep_level=$(mvn dependency:tree -DoutputType=dot -Dincludes=${array_i[1]} | grep -e "->" | cut -d ">" -f 2 | cut -d '"' -f 2 | cut -d ":" -f 1-2)
+        IFS=' ' read -a dependency_level <<< "$dep_level"
+        array_i+=("${dependency_level[0]}")
+        table_row="| "
+        counter=0
+        for j in "${array_i[@]}"
+        do
+            if [[ $counter == 0 ]]; then
+                table_row+="[$j](https://github.com/$GITHUB_REPOSITORY/security/dependabot/$j) | "
+                counter=$((counter+1))
+            elif [[ $counter == 1 ]]; then
+                table_row+="$j | "
+                counter=$((counter+1))
+            elif [[ $counter == 2 ]]; then
+                if [[ $j == "critical" ]] || [[ $j == "high" ]]; then
+                    table_row+="‼️ $j | "
+                else
+                    table_row+="$j | "
+                fi
+                counter=$((counter+1))
+            elif [[ $counter == 3 ]]; then
+                table_row+="$j | "
+                counter=$((counter+1))
+            elif [[ $counter == 4 ]]; then
+                if [[ $j = "null" ]]; then
+                    table_row+="  | "
+                else
+                    table_row+="$j | "
+                fi
+                counter=$((counter+1))
+            elif [[ $counter == 5 ]]; then
+                table_row+="$j | "
+                counter=$((counter+1))
+            elif [[ $counter == 6 ]]; then
+                table_row+="$j | "
+                counter=$((counter+1))
+            else
+                continue
+            fi
+        done
+        echo "$table_row" >> "$GITHUB_STEP_SUMMARY"
+    done
+}
+
+
 # $1 - "project.clj" or "deps.edn"
 if [[ -n $INPUT_DIRECTORY ]]; then
     cd "$GITHUB_WORKSPACE$INPUT_DIRECTORY" || exit
@@ -33,11 +86,25 @@ do
         dependency_tree_summary "projectclj" "$i"
         mv pom.xml projectclj/
         maven-dependency-submission-linux-x64 --token "$GITHUB_TOKEN" --repository "$GITHUB_REPOSITORY" --branch-ref "$GITHUB_REF" --sha "$GITHUB_SHA" --directory "${cljdir}/projectclj" --job-name "${INPUT_DIRECTORY}${i}/projectclj"
+        db_path="${cljdir}/projectclj/pom.xml"
+        db_path=${db_path:1}
+        {
+            echo "| Number | Package | Severity | GHSA | CVE | Patched in | Dependency level |"
+            echo "| --- | --- | --- | --- | --- | --- | --- |"
+        } >> "$GITHUB_STEP_SUMMARY"
+        vulnerabilities_summary "$db_path"
     else
         clojure -Spom
         mkdir depsedn
         dependency_tree_summary "depsedn" "$i"
         mv pom.xml depsedn/
         maven-dependency-submission-linux-x64 --token "$GITHUB_TOKEN" --repository "$GITHUB_REPOSITORY" --branch-ref "$GITHUB_REF" --sha "$GITHUB_SHA" --directory "${cljdir}/depsedn" --job-name "${INPUT_DIRECTORY}${i}/depsedn"
+        db_path="${cljdir}/depsedn/pom.xml"
+        db_path=${db_path:1}
+        {
+            echo "| Number | Package | Severity | GHSA | CVE | Patched in | Dependency level |"
+            echo "| --- | --- | --- | --- | --- | --- | --- |"
+        } >> "$GITHUB_STEP_SUMMARY"
+        vulnerabilities_summary "$db_path"
     fi
 done
