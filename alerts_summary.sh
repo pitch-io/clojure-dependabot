db9605e2825e0f3a539a62a9f73f5d783ee5b309
Luigi Gubello <luigi@pitch.io>
2024-03-01 09:53:25 +0100

    Minor improvements in summary output

--- alerts_summary.sh
+++ alerts_summary.sh
@@ -6,7 +6,7 @@ dependency_tree_summary () {
         cat dependency-tree.txt
     fi
     {
-        echo "### $INPUT_DIRECTORY$2"
+        echo "### $INPUT_DIRECTORY$i"
         echo "<details>"
         echo ""
         echo "\`\`\`"
