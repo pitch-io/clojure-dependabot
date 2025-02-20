938bc49b06dde868ab00a06f15767514f143ba46
Dan Lebrero <dlebrero@gmail.com>
2025-02-20 15:57:13 +0100

    GH_TOKEN i
    Trying to fix:
    
    ```
    Updating Dependabot alerts
    gh: To use GitHub CLI in a GitHub Actions workflow, set the GH_TOKEN environment variable. Example:
      env:
        GH_TOKEN: ${{ github.token }}
    ```

--- dependabot_alerts.sh
+++ dependabot_alerts.sh
@@ -3,6 +3,6 @@
 if [[ "$INPUT_VERBOSE" == true ]]; then
         echo "Updating Dependabot alerts"
 fi
-export GITHUB_TOKEN=$GITHUB_PAT
+export GH_TOKEN=$GITHUB_PAT
 sleep 10
 gh api -H "Accept: application/vnd.github+json" "/repos/$GITHUB_REPOSITORY/dependabot/alerts" --paginate > /tmp/dependabot_alerts.json || true
