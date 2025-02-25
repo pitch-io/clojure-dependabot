e51808d088f68f6b2338e2655d703c79e8862dce
Ryan White <ryan.white@lifecheq.co.za>
2025-02-25 14:57:19 +0000

    manually making output pom file like that generated
    If this works, a bit more work to make it automatic

--- pom_generator.clj
+++ pom_generator.clj
@@ -19,15 +19,46 @@
 
 (defn deps->pom [deps destination]
   (let [tags (xml/element
-              :project {}
+              :project {:xmlns "http://maven.apache.org/POM/4.0.0"
+                        :xmlns:xsi "http://www.w3.org/2001/XMLSchema-instance"
+                        :xsi:schemaLocation "http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"}
+              (xml/element :modelVersion {} "4.0.0")
+              (xml/element :packaging {} "jar")
+              (xml/element :groupId {} "utwig")
+              (xml/element :artifactId {} "utwig")
+              (xml/element :version {} "0.1.0")
+              (xml/element :name {} "utwig")
               (xml/element
                :dependencies {}
                (for [{dep-name :name :as dep} deps]
-                (xml/element
-                 :dependency {}
-                 (xml/element :groupId {} (str (namespace dep-name)))
-                 (xml/element :artifactId {} (str (name dep-name)))
-                 (xml/element :version {} (str (-> dep :coord :mvn/version)))))))]
+                 (xml/element
+                  :dependency {}
+                  (xml/element :groupId {} (str (namespace dep-name)))
+                  (xml/element :artifactId {} (str (name dep-name)))
+                  (xml/element :version {} (str (-> dep :coord :mvn/version))))))
+
+              (xml/element :build {}
+                           (xml/element :sourceDirectory {} "src-shared"))
+
+              (xml/element
+               :repositories {}
+               (xml/element
+                :repository {}
+                (xml/element :id {} "central")
+                (xml/element :url {} "https://repo1.maven.org/maven2"))
+
+               (xml/element
+                :repository {}
+                (xml/element :id {} "clojars")
+                (xml/element :url {} "https://clojars.org/repo"))
+
+               (xml/element
+                :repository {}
+                (xml/element :id {} "jboss")
+                (xml/element :url {} "https://repository.jboss.org/maven2"))
+
+               ))]
+
     (with-open [out-file (java.io.FileWriter. destination)]
       (xml/emit tags out-file))))
 
